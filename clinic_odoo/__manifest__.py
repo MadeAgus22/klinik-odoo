b06cfa47f87704d27028de7ec7e2587e453fbd82
Nama Anda <email@example.com>
2025-08-21 22:48:03 +0800

    menambahkan menu master tarif untuk obat

--- clinic_odoo/__manifest__.py
+++ clinic_odoo/__manifest__.py
@@ -31,6 +31,7 @@ Sistem klinik odoo dalam pengembangan akan terus berkembang mengikuti perkembang
         'views/mst_kelas_tarif_views.xml',
         'views/mst_produk_views.xml',
         'views/mst_tarif_tindakan_embed_views.xml',
+        'views/mst_tarif_obat_embed_views.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
